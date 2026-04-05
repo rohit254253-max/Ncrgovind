ed07aa7f45ed7f8ca4346339ed623d3dca29d542
rohit254253-max <rohit254253@gmail.com>
2026-04-05 13:07:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function callNow() {
+  window.location.href = "tel:+918076874066";
+}
+
+function whatsappNow() {
+  window.open("https://wa.me/918076874066");
+}
+
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth"
+  });
+}
